f40a402057cb30c8997b3379ec699c09343ca953
th3-m0th <alonsofernandezfernandez64@gmail.com>
2025-01-15 23:01:17 +0100

    how dare you not work

---
+++ Persona.js
@@ -0,0 +1,12 @@
+class Persona {
+    constructor(id, nombre, apellidos, foto, telefono, direccion, fechaNacimiento, numeroDepartamento) {
+        this.id = 0
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.foto = foto;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.numeroDepartamento = numeroDepartamento;
+    }
+}
--- script.js
+++ script.js
@@ -4,16 +4,28 @@ window.onload = getAll()
 
 function addPersona() {
     // recogemos los campos y los almacenamos en persona
-    var persona;
-    persona.nombre = document.getElementById("nombre");
-    persona.apellidos = document.getElementById("apellidos");
-    persona.foto = document.getElementById("foto");
-    persona.telefono = document.getElementById("telefono");
-    persona.direccion = document.getElementById("direccion");
-    persona.fechaNacimiento = document.getElementById("fechaNacimiento");
-    persona.numeroDepartamento = document.getElementById("numeroDepartamento");
-
+    var persona = new Persona();
+    persona.nombre = document.getElementById("nombre").value;
+    persona.apellidos = document.getElementById("apellidos").value;
+    persona.foto = document.getElementById("foto").value;
+    persona.telefono = document.getElementById("telefono").value;
+    persona.direccion = document.getElementById("direccion").value;
+    persona.fechaNacimiento = document.getElementById("fechaNacimiento").value;
+    persona.numeroDepartamento = document.getElementById("numeroDepartamento").value;
+    // comprobamos q todo guay
     console.log(persona);
+
+    var client = new XMLHttpRequest();
+    client.open("POST", URL);
+    client.onreadystatechange = () => {
+        if(client.readyState == 4 && client.status == 200) {
+            alert("Se ha agregado correctamente.");
+            getAll();
+        }
+    }
+    client.setRequestHeader("Content-Type", "application/json");
+    client.send(JSON.stringify(persona));
+
 }
 
 function deletePersona(id, event) {
